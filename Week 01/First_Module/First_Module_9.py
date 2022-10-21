89418660e9e53346934f4e1103827e0535f907c9
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-10-21 20:22:03 +0600

    First_Week

---
+++ Week 01/First_Module/First_Module_9.py
@@ -0,0 +1,26 @@
+""" 
+1. What is the python
+2. python instll
+3. vs code
+4. variables
+5. int
+6. string
+7. boolean
+8. type
+9. print
+10. f string
+11. input
+12. comment
+"""
+
+""" 
+Home work
+1. take three input from the user
+2. add the three inputs and display the sum
+3. take two number then show the multiplication
+4. take first name and last name as input and display the full name 
+5. take three inputs . and output will be like :
+"Hey I am ____. My age is ____. I love to eat ____."
+6. (advanced) take one input and display 4 times of that number
+7. (advanced) take degree C and convert the temperature to F
+"""
---
+++ Week 01/Second_Module/Second_Module_2.py
@@ -0,0 +1,22 @@
+# 6 types of comparison
+
+# < checks whether the left side of the symbol is smaller then the right side
+print(12 < 35)
+print(42 < 35)
+
+# > (checks biger or not)
+print(12 > 56)
+print(89 > 12)
+
+# >= (checks whether bigger or equal)
+print(12 >= 12)
+# <= (checks whether smaller or equal)
+print(9 <= 9)
+
+my_money = 45
+your_money = 65
+
+# == check whether both the values are equal
+print(my_money == your_money)
+# != check whether both the values are not equal
+print(my_money != your_money)
