6e5cc336e279f1f47f07fdea5f83bd1a9cbf3683
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-10-21 20:47:53 +0600

    First_Module

---
+++ Week 01/Third_Module/Third_Module_3.py
@@ -0,0 +1,14 @@
+'''
+Encrypt the following code so that no one can get your strategy.
+'''
+
+# data = 'firebaby'
+
+data = 'firebaby'
+shift = 1
+output = ''
+
+for character in data:
+    output += chr((ord(character) + shift - 97) % 26 + 97)
+
+print(output)
---
+++ Week 01/Third_Module/Third_Module_4.py
@@ -0,0 +1,16 @@
+'''
+Game : Guess a number
+
+requirment -
+* user instructions
+* delay
+
+formula - ((2x + 8)/ 2) - x
+'''
+
+x = input('Guess a number : ')
+x = input('Multiply 2 with your number')
+x = input('Add 8 with the result')
+x = input('Divide the whole result with 2')
+x = input('Substract the gussed number with the result')
+print('your guessed number is 4\n')
