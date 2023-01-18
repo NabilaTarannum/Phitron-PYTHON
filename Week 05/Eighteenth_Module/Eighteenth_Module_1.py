6124bbac42072bc146113a2b62741a04a8bed071
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2023-01-18 20:09:38 +0600

    Fifth_Week

--- Week 05/Eighteenth_Module/Eighteenth_Module_1.py
+++ Week 05/Eighteenth_Module/Eighteenth_Module_1.py
@@ -1,1 +1,16 @@
 """ Attributes Validation Using Assert """
+
+""" todo.txt ====>>
+
+1. Attributes Validation Using Assert
+2. Dynamic Attributes
+3. Create classes from json in files
+4. Multiply inheritance and MRO(Method Resolution Order) """
+
+""" validate.py =====>> """
+
+
+class Item:
+    def __init__(self, itemName, itemPrice) -> None:
+        self.itemName = itemName
+        self.itemPrice = itemPrice
