9795101237e506339fba5e9351d541cc9ea0eea8
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2023-01-18 22:12:39 +0600

    Fifth_Week

--- Week 05/Eighteenth_Module/Eighteenth_Module_1.py
+++ Week 05/Eighteenth_Module/Eighteenth_Module_1.py
@@ -12,10 +12,26 @@
 
 class Item:
     def __init__(self, itemName, itemPrice) -> None:
-        assert itemPrice >= 0
+        assert itemPrice >= 0, f"Error line 3, {itemPrice} is invalid"
         self.itemName = itemName
         self.itemPrice = itemPrice
 
 
-item = Item("Plate", -150)
+item = Item("Plate", 150)
 print(item.itemName, item.itemPrice)
+
+
+class Person:
+    def __init__(self, name, phone, age) -> None:
+        assert age >= 13, "Only greater than 13 is permitted"
+        assert len(phone) == 11, "invalid phone number"
+        self.name = name
+        self.phone = phone
+        self.age = age
+
+    def __repr__(self) -> str:
+        return f"{self.name} {self.phone} {self.age}"
+
+
+user = Person("Nabila Tarannum", "01234567890", 17)
+print(user)
