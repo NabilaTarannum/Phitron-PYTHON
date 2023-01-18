2ee7eba6b618c1288cdf6eff655a39ea75467d65
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2023-01-18 23:39:31 +0600

    Fifth_Week

--- Week 05/Eighteenth_Module/Eighteenth_Module_2.py
+++ Week 05/Eighteenth_Module/Eighteenth_Module_2.py
@@ -5,13 +5,30 @@
 
 
 class Item:
+    all = []
+
     def __init__(self, itemName, itemPrice) -> None:
-        self.itemName = itemName
-        self.itemPrice = itemPrice
+        self.__itemName = itemName
+        self.__itemPrice = itemPrice
+        self.all.append(self)
+
+    def __repr__(self) -> str:
+        return f"Item({self.itemName},{self.itemPrice})"
 
 
 item1 = Item("Bowl", 100)
 item2 = Item("Plate", 150)
+item1.__discount = 10
+item1._Item__itemName = "Bowl Broken"
+# print(item1.__discount)
 
+# item1.discount = 10
+# item2.offer = "60%"
+
+# Item.all.append(item1)
+# print(Item.all)
 print(item1.__dict__)
-print(item2.__dict__)
+# print(item2.__dict__)
+
+# print(item1.all)
+# print(item2.all)
